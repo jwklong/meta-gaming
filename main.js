c6fcbb07a4023c43e69fb30277aca802ff5e43f6
sussy layers dev <72522395+jwklong@users.noreply.github.com>
2022-10-31 17:46:56 +0000

    fix

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 function checkGoals() {
     completedGoals = 0
-    for (game of games) {
+    for (const key of Object.keys(games)) {
+        const game = games[key]
         const save = localStorage.getItem(game.save)
         if (save !== undefined) {
             save = game.saveParser(save)
         }
-        for (goal of game.goals) {
-            completedGoals += goal(save)
+        for (goal of Object.keys(game.goals)) {
+            completedGoals += game.goals[goal](save)
         }
     }
     return completedGoals
@@ -14,8 +15,8 @@ function checkGoals() {
 
 function totalGoals() {
     goals = 0
-    for (game of games) {
-        goals += game.goals.length
+    for (const key of Object.keys(games)) {
+        goals += games[key].goals.length
     }
     return goals
 }
